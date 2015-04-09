4471693aa4cbd63fc7be97112b4749a6056b18c7
Chris Ewbank <ce90@kent.ac.uk>
2015-04-09 23:20:44 +0100

    Add comments

--- src/org/djpc/Puzzle.java
+++ src/org/djpc/Puzzle.java
@@ -3,15 +3,20 @@ package org.djpc;
 import java.util.Arrays;
 import java.util.LinkedList;
 
-/**
- *
- */
+/** Object representing a puzzle configuration. */
 public class Puzzle {
 
+    /** 2D array representing the positions of tiles in the puzzle. */
     private final char[][] config;
+    /** The X position of the blank tile. */
     private int blankX;
+    /** The Y position of the blank tile. */
     private int blankY;
 
+    /**
+     * Construct a puzzle from its string representation.
+     * @param config The string representation of the puzzle configuration, eg "adddbadcbd_b".
+     */
     public Puzzle(String config) {
         int pos = 0;
         this.config = new char[4][3];
@@ -27,12 +32,27 @@ public class Puzzle {
         }
     }
 
-    private Puzzle(char[][] config, int blankX, int blankY) {
-        this.blankX = blankX;
-        this.blankY = blankY;
-        this.config = config;
+    /**
+     * Deep copies the original puzzle, then shifts the configuration by moving the blank tile the specified amount.
+     * @param orig The puzzle to clone.
+     * @param x The amount to move the blank tile in the X direction.
+     * @param y The amount to move the blank tile in the Y direction.
+     */
+    private Puzzle(Puzzle orig, int x, int y) {
+        config = orig.deepCloneConfig();
+
+        config[blankY][blankX] = config[blankY + y][blankX + x];
+        config[blankY + y][blankX + x] = '_';
+
+        blankX = orig.blankX + x;
+        blankY = orig.blankY + y;
     }
 
+    /**
+     * Checks each position to ensure that the tiles are identical. If this is true then the configurations are identical.
+     * @param other The object to check equality against.
+     * @return True if the object is a Puzzle, and it has the same tile configuration.
+     */
     public boolean equals(Object other) {
         if (other instanceof Puzzle) {
             Puzzle otherPuzzle = (Puzzle) other;
@@ -49,6 +69,10 @@ public class Puzzle {
         }
     }
 
+    /**
+     * Returns a string representation of the puzzle configuration.
+     * @return A string representation of the puzzle configuration spilt over 4 lines, eg "add \ndba \ndcb \nd_b \n"
+     */
     public String toString() {
         final StringBuilder stringBuilder = new StringBuilder(20);
         for (char[] row : config) {
@@ -60,33 +84,32 @@ public class Puzzle {
         return stringBuilder.toString();
     }
 
+    /**
+     * Generates the next possible valid configurations from the current configuration.
+     * @return A LinkedList containing the next possible valid configurations.
+     */
     public LinkedList<Puzzle> nextConfigs() {
         final LinkedList<Puzzle> nextConfigs = new LinkedList<>();
-        if (blankY > 0) {
-            nextConfigs.add(shiftTiles(0,-1));
+        if (blankY > 0) { // Don't move the blank tile up if its already on the top row.
+            nextConfigs.add(new Puzzle(this, 0,-1));
         }
-        if (blankY < 3) {
-            nextConfigs.add(shiftTiles(0,1));
+        if (blankY < 3) { // Don't move the blank tile down if its already on the bottom row.
+            nextConfigs.add(new Puzzle(this, 0,1));
         }
-        if (blankX > 0) {
-            nextConfigs.add(shiftTiles(-1,0));
+        if (blankX > 0) { // Don't move the blank tile left if its already on the leftmost row.
+            nextConfigs.add(new Puzzle(this, -1,0));
         }
-        if (blankX < 2) {
-            nextConfigs.add(shiftTiles(1,0));
+        if (blankX < 2) { // Don't move the blank tile right if its already on the rightmost row.
+            nextConfigs.add(new Puzzle(this, 1,0));
         }
 
         return nextConfigs;
     }
 
-    private Puzzle shiftTiles(int x, int y) {
-        final char[][] nextConfig = deepCloneConfig();
-
-        nextConfig[blankY][blankX] = nextConfig[blankY + y][blankX + x];
-        nextConfig[blankY + y][blankX + x] = '_';
-
-        return new Puzzle(nextConfig,blankX + x,blankY + y);
-    }
-
+    /**
+     * Deep clones the tile configuration.
+     * @return An exact copy of the tile configuration.
+     */
     private char[][] deepCloneConfig() {
         final char[][] nextConfig = new char[4][3];
 
--- src/org/djpc/PuzzleSolver.java
+++ src/org/djpc/PuzzleSolver.java
@@ -1,28 +1,42 @@
 package org.djpc;
 
+import com.sun.istack.internal.Nullable;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * Finds the shortest route between a two 11 tile puzzle configurations.
+ */
 public class PuzzleSolver {
 
+    /**
+     * Finds the shortest route between a two 11 tile puzzle configurations.
+     * @param filename The filename containing the start and destination configurations, eg "adddbadcbd_b2ad_dabdcbddb.txt"
+     */
     public PuzzleSolver(String filename) {
-        String[] split = filename.split("[.2]");
+        String[] split = filename.split("[.2]"); // Split the filename at '.' and '2' characters.
 
-        if (split.length != 3) {
+        if (split.length != 3) { // Check that this gives us 3 parts to the filename.
             System.err.println("Incorrect filename format: " + filename);
             System.exit(1);
         }
 
-        LinkedList<Puzzle> solution = iterativeDeepening(new Puzzle(split[0]), new Puzzle(split[1]));
+        List<Puzzle> solution = iterativeDeepening(new Puzzle(split[0]), new Puzzle(split[1]));
+
         String[] formattedOutput = formatOutput(solution);
+
+        // Print the result to the console.
         for (String line : formattedOutput) {
             System.out.println(line);
         }
         System.out.println(filename);
 
+        // Write the result out to file.
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             for (String line : formattedOutput) {
                 writer.append(line);
@@ -33,6 +47,11 @@ public class PuzzleSolver {
         }
     }
 
+    /**
+     * Finds the shortest route between a two 11 tile puzzle configurations.
+     * @param args Expects 1 argument, which should be the filename containing the start and destination configurations,
+     *             eg "adddbadcbd_b2ad_dabdcbddb.txt"
+     */
     public static void main(String[] args) {
         if (args.length != 1) {
             System.err.println("Expected 1 argument, got " + args.length);
@@ -41,7 +60,12 @@ public class PuzzleSolver {
         new PuzzleSolver(args[0]);
     }
 
-    private String[] formatOutput(LinkedList<Puzzle> solution) {
+    /**
+     * Formats a list of puzzle configurations into 4 lines of text.
+     * @param solution A route between two configurations.
+     * @return An array of strings, one for each line, containing the route between two puzzle configurations.
+     */
+    private String[] formatOutput(List<Puzzle> solution) {
         String[] lines = new String[4];
         Arrays.fill(lines, "");
 
@@ -55,37 +79,53 @@ public class PuzzleSolver {
         return lines;
     }
 
-    private LinkedList<Puzzle> iterativeDeepening(Puzzle start, Puzzle dest) {
-        for (int depth = 1; true; depth++) { // doubtful termination
+    /**
+     * Finds the shortest path between two puzzle configurations by incrementing the maximum depth of paths and
+     * attempting to find a path between configurations at each depth.
+     * @param start The start configuration.
+     * @param dest The destination configuration.
+     * @return A List containing the route between the configurations.
+     */
+    private List<Puzzle> iterativeDeepening(Puzzle start, Puzzle dest) {
+        for (int depth = 1; true; depth++) { // Loop forever, incrementing the maximum depth at each iteration.
             LinkedList<Puzzle> startList = new LinkedList<>();
             startList.add(start);
 
-            final LinkedList<Puzzle> route = depthFirstDevaVu(startList, dest, depth);
+            final List<Puzzle> route = depthFirstDevaVu(startList, dest, depth);
 
             if (route != null) {
-                System.out.println(depth);
-                return route; // fast exit
+                System.out.println(depth); // Print the length of the route.
+                return route; // If we have found a route return it.
             }
         }
     }
 
+    /**
+     * Attempt to find a route from the end of the current route to the destination configuration. Checks to make sure
+     * that it does not go back to the same configuration.
+     * @param route The previous configurations in the route.
+     * @param dest The destination configuration.
+     * @param depth The number of moves remaining.
+     * @return A route to the destination configuration, or null if a route is not found.
+     */
+    @Nullable
     private LinkedList<Puzzle> depthFirstDevaVu(LinkedList<Puzzle> route, Puzzle dest, int depth) {
         if (depth == 0) {
-            return null;
+            return null; // If we have no moves remaining, a route is not possible
         }
 
         Puzzle last = route.getLast();
         if (last.equals(dest)) {
-            return route;
+            return route; // If the last node in the route is the destination, we have found a route
         } else {
-            LinkedList<Puzzle> nextConfigs = last.nextConfigs();
+            LinkedList<Puzzle> nextConfigs = last.nextConfigs(); // Get the next possible moves
             for (Puzzle next : nextConfigs) {
-                if (!route.contains(next)) {
+                if (!route.contains(next)) { // Check if we have already visited this configuration
                     LinkedList<Puzzle> nextRoute = (LinkedList<Puzzle>) route.clone();
                     nextRoute.add(next);
-                    LinkedList<Puzzle> wholeRoute = depthFirstDevaVu(nextRoute, dest, depth - 1);
+                    LinkedList<Puzzle> wholeRoute = depthFirstDevaVu(nextRoute, dest, depth - 1); // Try to find a route from this configuration to the destination
                     if (wholeRoute != null) {
-                        return wholeRoute;
+                        return wholeRoute; // Return a route that has been found
                     }
                 }
             }
